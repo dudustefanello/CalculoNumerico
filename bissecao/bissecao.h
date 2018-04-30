bde0bd7860eff09e2c7581f95c123b5f06563b75
Eduardo <eduardostefanello3@gmail.com>
2018-04-29 21:36:02 -0300

    Implementação do Erro para bisseção estimativa

--- bissecao/bissecao.h
+++ bissecao/bissecao.h
@@ -5,23 +5,34 @@ typedef struct retorno{
   int iteracoes;
 }retorno;
 
+typedef retorno (*bissecoes)(double a, double b, double (*funcao)(double));
+
 int sinal(double x1, double x2){
   if (x1 * x2 < 0) return -1;
   if (x1 * x2 > 0) return 1;
   return 0;
 }
 
+double erro(double a, double b){
+  return fabs((a - b) / a);
+}
+
 int iteracoes(double a, double b){
   return ceil((log10(b - a) - log10(EPSON)) / log10(2)) - 1;
 }
 
-retorno bissecao1(double a, double b, double (*funcao)(double)) {
+retorno bissecaoEstimativa(double a, double b, double (*funcao)(double)) {
   retorno ret;
+  double x;
 
+  ret.y = 1;
   ret.iteracoes = iteracoes(a, b);
 
   for (int i = 0; i <= ret.iteracoes; i++) {
+    x = ret.y;
     ret.y = a + (b - a) / 2;
+
+    ret.erro = erro(ret.y, x);
 
     if (sinal(funcao(a), funcao(ret.y)) <= 0) b = ret.y;
     else a = ret.y;
@@ -29,20 +40,17 @@ retorno bissecao1(double a, double b, double (*funcao)(double)) {
   return ret;
 }
 
-double erro(double a, double b){
-  return fabs((a - b) / a);
-}
-
-retorno bissecao2(double a, double b, double (*funcao)(double)) {
+retorno bissecaoErro(double a, double b, double (*funcao)(double)) {
   retorno ret;
   double x;
+
   ret.y = 1;
 
   for (ret.iteracoes = 0; 1; ret.iteracoes++) {
     x = ret.y;
     ret.y = a + (b - a) / 2;
 
-    if (ret.y < EPSON) break;
+    if (fabs(funcao(ret.y)) < EPSON) break;
 
     ret.erro = erro(ret.y, x);
     if (ret.erro < EPSON) break;
@@ -52,3 +60,7 @@ retorno bissecao2(double a, double b, double (*funcao)(double)) {
   }
   return ret;
 }
+
+bissecoes bissecao[] = {
+  bissecaoEstimativa, bissecaoErro
+};
--- bissecao/main.cpp
+++ bissecao/main.cpp
@@ -3,23 +3,25 @@
 using namespace std;
 
 void imprimir(tipoEntrada i){
-  retorno ret = bissecao1(i.a, i.b, funcao[i.f - 1]);
+  retorno ret = bissecao[0](i.a, i.b, funcao[i.f - 1]);
 
   printf("Funcao %d: \n", i.f);
   printf("Intervalo: [%.3lf, %.3lf]\n", i.a, i.b);
   printf("Iteracoes: %d\n\n", ret.iteracoes);
 
   printf("x    = %.15lf\n", ret.y);
-  printf("f(x) = %.15lf\n\n", funcao[i.f - 1](ret.y));
+  printf("f(x) = %.15lf\n", funcao[i.f - 1](ret.y));
+  printf("Erro = %.15lf\n\n", ret.erro);
 
-  ret = bissecao2(i.a, i.b, funcao[i.f - 1]);
+  ret = bissecao[1](i.a, i.b, funcao[i.f - 1]);
 
   printf("Funcao %d: \n", i.f);
   printf("Intervalo: [%.3lf, %.3lf]\n", i.a, i.b);
   printf("Iteracoes: %d\n\n", ret.iteracoes);
 
   printf("x    = %.15lf\n", ret.y);
-  printf("f(x) = %.15lf\n\n", funcao[i.f - 1](ret.y));
+  printf("f(x) = %.15lf\n", funcao[i.f - 1](ret.y));
+  printf("Erro = %.15lf\n\n", ret.erro);
 }
 
 int main(int argc, char const *argv[]) {
