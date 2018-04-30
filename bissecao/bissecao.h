65435ed5678f63f5cd1f37ba01bdc755fa26a238
Eduardo <eduardostefanello3@gmail.com>
2018-04-29 21:22:52 -0300

    Segunda implementação bisseção com e sem estimativa.

--- bissecao/bissecao.h
+++ bissecao/bissecao.h
@@ -11,18 +11,43 @@ int sinal(double x1, double x2){
   return 0;
 }
 
-int iteracoes(double b, double a){
-  return ceil((log10(b - a) - log10(EPSON)) / log10(2));
+int iteracoes(double a, double b){
+  return ceil((log10(b - a) - log10(EPSON)) / log10(2)) - 1;
 }
 
-retorno bissecao1(double b, double a, double (*funcao)(double)) {
+retorno bissecao1(double a, double b, double (*funcao)(double)) {
   retorno ret;
 
   ret.iteracoes = iteracoes(a, b);
 
   for (int i = 0; i <= ret.iteracoes; i++) {
     ret.y = a + (b - a) / 2;
+
     if (sinal(funcao(a), funcao(ret.y)) <= 0) b = ret.y;
+    else a = ret.y;
+  }
+  return ret;
+}
+
+double erro(double a, double b){
+  return fabs((a - b) / a);
+}
+
+retorno bissecao2(double a, double b, double (*funcao)(double)) {
+  retorno ret;
+  double x;
+  ret.y = 1;
+
+  for (ret.iteracoes = 0; 1; ret.iteracoes++) {
+    x = ret.y;
+    ret.y = a + (b - a) / 2;
+
+    if (ret.y < EPSON) break;
+
+    ret.erro = erro(ret.y, x);
+    if (ret.erro < EPSON) break;
+
+    if (sinal(funcao(a), funcao(ret.y)) < 0) b = ret.y;
     else a = ret.y;
   }
   return ret;
--- bissecao/main.cpp
+++ bissecao/main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 void imprimir(tipoEntrada i){
-  retorno ret = bissecao(i.a, i.b, funcao[i.f - 1]);
+  retorno ret = bissecao1(i.a, i.b, funcao[i.f - 1]);
+
+  printf("Funcao %d: \n", i.f);
+  printf("Intervalo: [%.3lf, %.3lf]\n", i.a, i.b);
+  printf("Iteracoes: %d\n\n", ret.iteracoes);
+
+  printf("x    = %.15lf\n", ret.y);
+  printf("f(x) = %.15lf\n\n", funcao[i.f - 1](ret.y));
+
+  ret = bissecao2(i.a, i.b, funcao[i.f - 1]);
 
   printf("Funcao %d: \n", i.f);
   printf("Intervalo: [%.3lf, %.3lf]\n", i.a, i.b);
